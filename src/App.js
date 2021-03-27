5de314f714e0ad0872231335d06134c38f367fd1
Malcolm <malc_isa@protonmail.com>
2021-03-27 21:23:59 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return null;
+  //return <h1>Bekkstagram</h1>;
+  //const MinKomponent = (props) => {
+    return React.createElement("h1", {}, "Hei verden");
 }
 
 export default App;
