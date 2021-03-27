c5bc95ca0a19f0af2124c25d5a253e51233e6f74
Malcolm <malc_isa@protonmail.com>
2021-03-27 22:02:13 +0100

    opptil opggave 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
-function App() {
+function Header() {
+  return <h1>Bekkstagram</h1>;
+}
+
+function Image(props) {
+  return <img src={props.src} alt={props.alt} className="image" />;
+}
+
   //return <h1>Bekkstagram</h1>;
   //const MinKomponent = (props) => {
-    return React.createElement("h1", {}, "Hei verden");
-}
+  //return React.createElement("h1", {}, "Hei verden");
+  function App() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="images">
+          {images.map(image => (
+            <Image key={image.id} src={image.url} alt={image.description} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 
 export default App;
